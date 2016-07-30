5743545e98109f6a3cbc89d09c74c76a61cfbc91
olagz <olagodavivid@gmail.com>
2016-07-30 23:07:22 +0300

    intro to python part 3

---
+++ intro_a_basics/b_syntax.py
@@ -0,0 +1,22 @@
+'''
+@summary: Python Intro - Syntax
+'''
+
+# ----------------------
+# Indentation
+# ----------------------
+
+# python uses indentation instead of brackets (can be either space or tab)
+if True:
+    pass
+    if False:
+        pass
+    elif True:
+        pass
+    else:
+        pass
+
+# you can write code after the :
+if True: print "one liner also works"
+
+            
