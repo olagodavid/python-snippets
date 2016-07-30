fe5e43640ca88cd4099418d7173a56c8ab0c0f47
olagz <olagodavivid@gmail.com>
2016-07-30 22:55:56 +0300

    initial

---
+++ tutorials/mod_logger.py
@@ -0,0 +1,9 @@
+import datetime
+
+def log_warning(msg):
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    print "%s, '%s'" % (now, msg)
+
+if __name__ == '__main__':
+    print "#########################################"
+    log_warning("test now")
