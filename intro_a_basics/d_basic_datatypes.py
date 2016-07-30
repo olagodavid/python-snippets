621707902bdc95f0e1bdb1c04f7c1577b63081a8
olagz <olagodavivid@gmail.com>
2016-07-30 23:07:59 +0300

    intro to python part 4

---
+++ intro_a_basics/d_basic_datatypes.py
@@ -0,0 +1,29 @@
+'''
+@summary: Python Intro - Basic DataTypes
+'''
+# ----------------------
+# Assignments
+# ----------------------
+# first assignment creates name which references value in memory, types are inferred
+aint = 5
+print type(aint)
+aint = True
+print type(aint)
+
+# multiple assignments
+cstr = dstr = 5 # both cstr and dstr will contain the value: 5
+dstr = 6
+print cstr
+
+# ---------------------------
+# Numbers
+# ---------------------------
+a = 5 + 5  # int
+b = 2.5  # float
+
+# ---------------------------
+# Boolean
+# ---------------------------
+a = True
+b = False
+
